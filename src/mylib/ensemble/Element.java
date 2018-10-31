176c9aa7f5aabed5bc83378956ef9bcd47ddcb1f
monsio <monsio.b@gmail.com>
2018-10-31 16:23:06 +0100

    ajout packages

--- src/mylib/ensemble/Element.java
+++
@@ -1,7 +0,0 @@
-package mylib.ensemble;
-
-/**
- * Created by monsio on 15/07/16.
- */
-public interface Element {
-}
--- src/mylib/graph/GraphMatrice.java
+++
@@ -1,7 +0,0 @@
-package mylib.graph;
-
-/**
- * Created by monsio on 05/07/16.
- */
-public class GraphMatrice {
-}
